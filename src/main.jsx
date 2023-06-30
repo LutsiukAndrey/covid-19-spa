a7971f08648dea7165995e08482183377ab166c3
Andrii <lutsiukandrey1@gmail.com>
2023-06-30 19:53:31 +0300

    add base name

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <BrowserRouter>
+  <BrowserRouter basename="covid-19-spa">
     <App />
   </BrowserRouter>
   // </React.StrictMode>
